80c67e783db4129752a6011467b590bb880c47fd
Alijah Green <atra2396@gmail.com>
2018-03-07 14:57:40 -0500

    Moved some stuff around in the template views folder

---
+++ spydr/resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+
+    <head>
+        <link href="{{ asset('css/darkly.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('js/app.css') }}" rel="stylesheet">
+    </head>
+
+
+
+</html>
