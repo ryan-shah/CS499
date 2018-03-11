72f90cf80eb2b4f58474dee8a8ca31efe0cfcb98
Natini8 <insertclevertitle92@gmail.com>
2018-03-11 18:07:50 -0400

    Testing sidebar format

--- spydr/resources/views/partials/sidebar.blade.php
+++ spydr/resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     <form class="form-inline">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <input class="form-control" type="text" placeholder="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
 </div>
