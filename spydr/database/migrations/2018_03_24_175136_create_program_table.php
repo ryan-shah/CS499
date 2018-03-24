d937ad9f65e99a131eb751daf4a83cb133e66e5f
Alijah Green <atra2396@gmail.com>
2018-03-24 14:21:06 -0400

    Added migrations for database creation. They've all been run.

--- spydr/database/migrations/2018_03_24_175136_create_program_table.php
+++ spydr/database/migrations/2018_03_24_175136_create_program_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProgramTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('programs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('estimated_memory_usage');
+            $table->unsignedInteger('estimated_time');
+            $table->string('path');
+            $table->string('command_line');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('program');
     }
 }
