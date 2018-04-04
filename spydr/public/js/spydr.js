4aea33477e46a9be8bd5165f9307543c9c77c05e
Alijah Green <atra2396@gmail.com>
2018-04-04 13:48:17 -0400

    Rudimentary AJAX requests

--- spydr/public/js/spydr.js
+++ spydr/public/js/spydr.js
@@ -1,6 +1,25 @@
-function addProgram(){
-    alert("Program jazz happens");
-}
+$(document).ready(function () {
+
+    $.ajaxSetup({   // Laravel requires this in order to make post requests
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    function addProgram() {
+        $(".savestuff").click(function(){
+            var data = {
+                blah: "Yo"
+            };
+            $.post("/", data, function (response) {
+                console.log(response);
+            });
+        });
+    }
+
+    addProgram();
+});
+
 
 function editProgram(program_id){
     alert("Editing program with ID " + program_id);
