c8e7ff93ec5f36c504b9f5ea0f975a8941a9b986
ryan-shah <ryan91396@gmail.com>
2018-04-11 11:23:41 -0400

    now updates program memory usage / time after execution

--- threadTesting/spydr.cpp
+++ threadTesting/spydr.cpp
@@ -1,14 +1,16 @@
 #include "program.h"
 #include "schedule.h"
-
+#include "unistd.h"
 using namespace std;
 
 int main() {
 
+while(1) {
+
 	Schedule s;
 	s.get_scripts();
-	s.hour = 13;
-	s.min = 19;
+	s.hour = 11;
+	s.min = 20;
 	s.days.push_back("wednesday");
 
 	s.programs[0].dependencies.push_back(&s.programs[1]);
@@ -16,6 +18,15 @@ int main() {
 	if(s.timeToRun()) {
 		cout << "time to run" << endl;
 		s.run();
+		if(s.timeToRun() ) {
+			cout << "finished in < 10 mins, waiting for another 10 to avoid running twice" << endl;
+			sleep(10*60);
+		}
+
+	} else {
+		sleep(5 * 60);
 	}
+
+}
 
 }
