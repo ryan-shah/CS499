624e8c2f6fad2430ba120baec59b8d35c6a783c6
ryan-shah <ryan91396@gmail.com>
2018-04-11 10:44:43 -0400

    comments

--- threadTesting/spydr.cpp
+++ threadTesting/spydr.cpp
@@ -8,8 +8,10 @@ int main() {
 	Schedule s;
 	s.get_scripts();
 	s.hour = 13;
-	s.min = 25;
+	s.min = 19;
 	s.days.push_back("wednesday");
+
+	s.programs[0].dependencies.push_back(&s.programs[1]);
 
 	if(s.timeToRun()) {
 		cout << "time to run" << endl;
